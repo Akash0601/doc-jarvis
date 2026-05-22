4dbfe0abca63896b83248e668ad81090ee92b883
Akash Shaw <akash111shaw@gmail.com>
2026-05-22 16:31:21 +0530

    chore: initialize Spring Boot project with PostgreSQL and YAML config

---
+++ src/main/java/com/docjarvis/DocjarvisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docjarvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocjarvisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocjarvisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docjarvis/DocjarvisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docjarvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocjarvisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
